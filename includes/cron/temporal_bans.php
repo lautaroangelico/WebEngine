d3d0a7fd6511ea0a721e49f39d87062ace31b555
lautaroangelico <angelico.lautaro@gmail.com>
2017-07-19 05:52:09 -0700

    Fixed compatibility issues

--- includes/cron/temporal_bans.php
+++ includes/cron/temporal_bans.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * WebEngine
- * http://muengine.net/
+ * WebEngine CMS
+ * https://webenginecms.org/
  * 
- * @version 1.0.9
+ * @version 1.0.9.6
  * @author Lautaro Angelico <http://lautaroangelico.com/>
  * @copyright (c) 2013-2017 Lautaro Angelico, All Rights Reserved
  * 
@@ -22,17 +22,18 @@ $dir_path = str_replace('\\','/',dirname(dirname(__FILE__))).'/';
 // Load WebEngine
 include($dir_path . 'webengine.php');
 
-$temporalBans = $dB->query_fetch("SELECT * FROM WEBENGINE_BANS");
+$database = (config('SQL_USE_2_DB',true) ? $dB2 : $dB);
+
+$temporalBans = $database->query_fetch("SELECT * FROM WEBENGINE_BANS");
 if(is_array($temporalBans)) {
 	foreach($temporalBans as $tempBan) {
 		$banTimestamp = $tempBan['ban_days']*86400+$tempBan['ban_date'];
 		if(time() > $banTimestamp) {
 			// lift ban
-			$db = (config('SQL_USE_2_DB',true) ? $dB2 : $dB);
-			$unban = $db->query("UPDATE "._TBL_MI_." SET "._CLMN_BLOCCODE_." = 0 WHERE "._CLMN_USERNM_." = ?", array($tempBan['account_id']));
+			$unban = $database->query("UPDATE "._TBL_MI_." SET "._CLMN_BLOCCODE_." = 0 WHERE "._CLMN_USERNM_." = ?", array($tempBan['account_id']));
 			if($unban) {
-				$dB->query("DELETE FROM WEBENGINE_BAN_LOG WHERE account_id = ?", array($tempBan['account_id']));
-				$dB->query("DELETE FROM WEBENGINE_BANS WHERE account_id = ?", array($tempBan['account_id']));
+				$database->query("DELETE FROM WEBENGINE_BAN_LOG WHERE account_id = ?", array($tempBan['account_id']));
+				$database->query("DELETE FROM WEBENGINE_BANS WHERE account_id = ?", array($tempBan['account_id']));
 			}
 		}
 	}

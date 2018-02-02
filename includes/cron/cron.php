7f0caaca09f07423340860d4133be63f4acdf944
lautaroangelico <angelico.lautaro@gmail.com>
2018-02-01 21:47:20 -0800

    fix for issue #19

--- includes/cron/cron.php
+++ includes/cron/cron.php
@@ -3,19 +3,19 @@
  * WebEngine CMS
  * https://webenginecms.org/
  * 
- * @version 1.0.9.8
- * @author Lautaro Angelico <http://lautaroangelico.com/>
- * @copyright (c) 2013-2017 Lautaro Angelico, All Rights Reserved
+ * @version 1.0.9.9
+ * @author Lautaro Angelico <https://lautaroangelico.com/>
+ * @copyright (c) 2013-2018 Lautaro Angelico, All Rights Reserved
  * 
  * Licensed under the MIT license
- * http://opensource.org/licenses/MIT
+ * https://opensource.org/licenses/MIT
  */
 
 // access
 define('access', 'cron');
 
 // Load WebEngine
-include(str_replace('\\','/',dirname(dirname(__FILE__))).'/' . 'webengine.php');
+if(!@include_once(str_replace('\\','/',dirname(dirname(__FILE__))).'/' . 'webengine.php')) die('Failed to load WebEngine CMS.');
 
 // Load Cache Data
 $cacheDATA = LoadCacheData('cron.cache');

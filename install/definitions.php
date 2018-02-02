e9d14b5ecced9b3aafa674a6b40242627ceb4a6b
lautaroangelico <angelico.lautaro@gmail.com>
2018-02-01 22:00:19 -0800

    updated webengine version and removed cron protected

--- install/definitions.php
+++ install/definitions.php
@@ -3,12 +3,12 @@
  * WebEngine CMS
  * https://webenginecms.org/
  * 
- * @version 1.0.9.8
- * @author Lautaro Angelico <http://lautaroangelico.com/>
- * @copyright (c) 2013-2017 Lautaro Angelico, All Rights Reserved
+ * @version 1.0.9.9
+ * @author Lautaro Angelico <https://lautaroangelico.com/>
+ * @copyright (c) 2013-2018 Lautaro Angelico, All Rights Reserved
  * 
  * Licensed under the MIT license
- * http://opensource.org/licenses/MIT
+ * https://opensource.org/licenses/MIT
  */
 
 if(!defined('access') or !access or access != 'install') die();
@@ -17,7 +17,7 @@ if(!defined('access') or !access or access != 'install') die();
  * INSTALLER_VERSION
  * installer version (do not change)
  */
-define('INSTALLER_VERSION', '1.0.9.8');
+define('INSTALLER_VERSION', '1.0.9.9');
 
 $install['PDO_DSN'] = array(
 	1 => 'dblib',
@@ -64,17 +64,17 @@ $install['step_list'] = array(
 
 $install['cron_jobs'] = array(
 	// cron_name,cron_description,cron_file_run,cron_run_time,cron_status,cron_protected,cron_file_md5
-	array('Levels Ranking','Scheduled task to update characters level ranking','levels_ranking.php','43200','1','1'),
-	array('Resets Ranking','Scheduled task to update characters reset ranking','resets_ranking.php','43200','1','1'),
-	array('Killers Ranking','Scheduled task to update top killers ranking','killers_ranking.php','43200','1','1'),
-	array('Master Level Ranking','Scheduled task to update characters master level ranking','masterlevel_ranking.php','43200','1','1'),
-	array('Guilds Ranking','Scheduled task to update top guilds ranking','guilds_ranking.php','43200','1','1'),
-	array('Grand Resets Ranking','Scheduled task to update characters grand reset ranking','grandresets_ranking.php','43200','1','1'),
-	array('Online Ranking','Scheduled task to update top online ranking','online_ranking.php','43200','1','1'),
-	array('Gens Ranking','Scheduled task to update gens ranking','gens_ranking.php','43200','1','1'),
-	array('PvP Last Stand Ranking','Scheduled task to update characters pvp last stand ranking','pvplaststand_ranking.php','43200','1','1'),
-	array('Votes Ranking','Scheduled task to update vote rankings','votes_ranking.php','43200','1','1'),
-	array('Castle Siege Owner','Saves castle siege guild owner name and logo in cache','castle_siege.php','3600','1','1'),
-	array('Ban System','Scheduled task to lift temporal bans','temporal_bans.php','3600','1','1'),
-	array('Server Info','Scheduled task to update the sidebar statistics information','server_info.php','300','1','1'),
+	array('Levels Ranking','Scheduled task to update characters level ranking','levels_ranking.php','43200','1','0'),
+	array('Resets Ranking','Scheduled task to update characters reset ranking','resets_ranking.php','43200','1','0'),
+	array('Killers Ranking','Scheduled task to update top killers ranking','killers_ranking.php','43200','1','0'),
+	array('Master Level Ranking','Scheduled task to update characters master level ranking','masterlevel_ranking.php','43200','1','0'),
+	array('Guilds Ranking','Scheduled task to update top guilds ranking','guilds_ranking.php','43200','1','0'),
+	array('Grand Resets Ranking','Scheduled task to update characters grand reset ranking','grandresets_ranking.php','43200','1','0'),
+	array('Online Ranking','Scheduled task to update top online ranking','online_ranking.php','43200','1','0'),
+	array('Gens Ranking','Scheduled task to update gens ranking','gens_ranking.php','43200','1','0'),
+	array('PvP Last Stand Ranking','Scheduled task to update characters pvp last stand ranking','pvplaststand_ranking.php','43200','1','0'),
+	array('Votes Ranking','Scheduled task to update vote rankings','votes_ranking.php','43200','1','0'),
+	array('Castle Siege Owner','Saves castle siege guild owner name and logo in cache','castle_siege.php','3600','1','0'),
+	array('Ban System','Scheduled task to lift temporal bans','temporal_bans.php','3600','1','0'),
+	array('Server Info','Scheduled task to update the sidebar statistics information','server_info.php','300','1','0'),
 );

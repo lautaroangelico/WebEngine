6bd1e979b16a8163687ecb99976106e7a849de3c
Lautaro Angelico <angelico.lautaro@gmail.com>
2019-01-15 13:47:07 -0800

    added animated registration sidebar banner

--- templates/redzone/inc/modules/sidebar.php
+++ templates/redzone/inc/modules/sidebar.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * WebEngine
- * http://muengine.net/
+ * WebEngine CMS
+ * https://webenginecms.org/
  * 
- * @version 1.0.9
+ * @version 1.1.0
  * @author Lautaro Angelico <http://lautaroangelico.com/>
- * @copyright (c) 2013-2017 Lautaro Angelico, All Rights Reserved
+ * @copyright (c) 2013-2019 Lautaro Angelico, All Rights Reserved
  * 
  * Licensed under the MIT license
  * http://opensource.org/licenses/MIT
@@ -13,25 +13,34 @@
 
 # Login block
 if(!isLoggedIn()) {
-echo '<div class="panel panel-sidebar">';
-	echo '<div class="panel-heading">';
-		echo '<h3 class="panel-title">'.lang('module_titles_txt_2',true).' <a href="'.__BASE_URL__.'forgotpassword" class="btn btn-primary btn-xs pull-right">'.lang('login_txt_4',true).'</a></h3>';
+	echo '<div class="panel panel-sidebar">';
+		echo '<div class="panel-heading">';
+			echo '<h3 class="panel-title">'.lang('module_titles_txt_2',true).' <a href="'.__BASE_URL__.'forgotpassword" class="btn btn-primary btn-xs pull-right">'.lang('login_txt_4',true).'</a></h3>';
+		echo '</div>';
+		echo '<div class="panel-body">';
+			echo '<form action="'.__BASE_URL__.'login" method="post">';
+				echo '<div class="form-group">';
+					echo '<input type="text" class="form-control" id="loginBox1" name="webengineLogin_user" required>';
+				echo '</div>';
+				echo '<div class="form-group">';
+					echo '<input type="password" class="form-control" id="loginBox2" name="webengineLogin_pwd" required>';
+				echo '</div>';
+				echo '<button type="submit" name="webengineLogin_submit" value="submit" class="btn btn-primary">'.lang('login_txt_3',true).'</button>';
+			echo '</form>';
+		echo '</div>';
 	echo '</div>';
-	echo '<div class="panel-body">';
-		echo '<form action="'.__BASE_URL__.'login" method="post">';
-			echo '<div class="form-group">';
-				echo '<input type="text" class="form-control" id="loginBox1" name="webengineLogin_user" required>';
-			echo '</div>';
-			echo '<div class="form-group">';
-				echo '<input type="password" class="form-control" id="loginBox2" name="webengineLogin_pwd" required>';
-			echo '</div>';
-			echo '<button type="submit" name="webengineLogin_submit" value="submit" class="btn btn-primary">'.lang('login_txt_3',true).'</button>';
-		echo '</form>';
-	echo '</div>';
-echo '</div>';
 
-# join now banner
-echo '<div class="sidebar-banner"><a href="'.__BASE_URL__.'register"><img src="'.__PATH_TEMPLATE_IMG__.'register_sidebar_banner.jpg"/></a></div>';
+	# join now banner
+	echo '<div style="margin:20px 0px;">';
+		echo '<a href="'.__BASE_URL__.'register">';
+			echo '<video width="300" height="100" preload="true" autoplay="" loop="" muted="">';
+				echo '<source src="'.__PATH_TEMPLATE_IMG__.'video/register_button.webm" type="video/webm">';
+				echo '<source src="'.__PATH_TEMPLATE_IMG__.'video/register_button.ogg" type="video/ogg">';
+				echo '<source src="'.__PATH_TEMPLATE_IMG__.'video/register_button.mp4" type="video/mp4">';
+				echo '<img src="'.__PATH_TEMPLATE_IMG__.'register_sidebar_banner.jpg"/>';
+			echo '</video>';
+		echo '</a>';
+	echo '</div>';
 }
 
 

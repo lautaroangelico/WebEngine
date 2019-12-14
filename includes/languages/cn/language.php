7a89eb586bba33820c1dc9b9d5999becbd18b1bf
Nemo Ma <nemoma@nmonolith.com>
2019-12-14 17:48:58 -0500

    Add Simp.Chinese language translation

---
+++ includes/languages/cn/language.php
@@ -0,0 +1,432 @@
+<?php
+/**
+ * WebEngine CMS
+ * https://webenginecms.org/
+ * 
+ * @version 1.2.0
+ * @author Lautaro Angelico <http://lautaroangelico.com/>
+ * @copyright (c) 2013-2019 Lautaro Angelico, All Rights Reserved
+ * 
+ * Licensed under the MIT license
+ * http://opensource.org/licenses/MIT
+ */
+
+# Translated by nemoma
+# general
+$lang['server_time'] = '服务器时间';
+$lang['user_time'] = '本地时间';
+$lang['switch_lang'] = '语言更改';
+
+# menu and navbar
+$lang['menu_txt_1'] = '主页';
+$lang['menu_txt_2'] = '论坛';
+$lang['menu_txt_3'] = '注册';
+$lang['menu_txt_4'] = '登录';
+$lang['menu_txt_5'] = '用户面板';
+$lang['menu_txt_6'] = '登出';
+$lang['menu_txt_7'] = '下载';
+$lang['menu_txt_8'] = '捐助';
+$lang['menu_txt_9'] = '投票领赏';
+$lang['menu_txt_10'] = '排行榜';
+$lang['menu_txt_11'] = '服务器信息';
+
+# module titles
+$lang['module_titles_txt_1'] = '注册一个新帐户';
+$lang['module_titles_txt_2'] = '账户登录';
+$lang['module_titles_txt_3'] = '用户面板';
+$lang['module_titles_txt_4'] = '我的账户';
+$lang['module_titles_txt_5'] = '更改Email地址';
+$lang['module_titles_txt_6'] = '更改密码';
+$lang['module_titles_txt_7'] = '投票领赏';
+$lang['module_titles_txt_8'] = '下载客户端';
+$lang['module_titles_txt_9'] = '使用条约';
+$lang['module_titles_txt_10'] = '排行榜';
+$lang['module_titles_txt_11'] = '捐助/购买货币';
+$lang['module_titles_txt_12'] = '人物转生';
+$lang['module_titles_txt_13'] = '清除红名';
+$lang['module_titles_txt_14'] = '常见问题（FAQ）';
+$lang['module_titles_txt_15'] = '忘记密码';
+$lang['module_titles_txt_16'] = '人物位置重置';
+$lang['module_titles_txt_17'] = '服务器信息';
+$lang['module_titles_txt_18'] = '数值修正';
+$lang['module_titles_txt_19'] = '技能树重置';
+$lang['module_titles_txt_20'] = 'Email地址验证';
+$lang['module_titles_txt_21'] = 'PayPal（贝宝）';
+$lang['module_titles_txt_22'] = 'Super Rewards';
+$lang['module_titles_txt_23'] = 'Western Union（西联）';
+$lang['module_titles_txt_24'] = 'Master Key 找回';
+$lang['module_titles_txt_25'] = '人物加点';
+$lang['module_titles_txt_26'] = '联系我们';
+$lang['module_titles_txt_27'] = 'PagSeguro';
+$lang['module_titles_txt_28'] = '购买Zen（游戏内金）';
+$lang['module_titles_txt_29'] = '攻城信息';
+
+# usercp
+$lang['usercp_menu_title'] = '用户控制面板';
+$lang['usercp_menu_txt_1'] = '我的账户';
+$lang['usercp_menu_txt_2'] = '人物转生';
+$lang['usercp_menu_txt_3'] = '清除红名';
+$lang['usercp_menu_txt_4'] = '投票领赏';
+$lang['usercp_menu_txt_5'] = '捐助购买点数';
+$lang['usercp_menu_txt_6'] = '位置重置';
+$lang['usercp_menu_txt_7'] = 'V.I.P. 系统';
+$lang['usercp_menu_txt_8'] = '数值修正';
+$lang['usercp_menu_txt_9'] = '重置技能树';
+$lang['usercp_menu_txt_10'] = '加点';
+$lang['usercp_menu_txt_11'] = '购买Zen';
+
+# success
+$lang['success_1'] = '你的账户已经成功创建！ 3秒钟后将会转移到登陆界面……';
+$lang['success_2'] = '你的账户密码已经更新。';
+$lang['success_3'] = '我们已经往你的邮箱发送了一封验证邮件。 验证后你的密码将会更新。';
+$lang['success_4'] = '已将你的新密码通过电邮发动到了你的邮箱！ （如果没收到请查看垃圾邮件文件夹）';
+$lang['success_5'] = '你的账户密码已经修改！';
+$lang['success_6'] = '我们已经往你的邮箱发送了一封包含找回账户链接的邮件！（如果没收到请查看垃圾邮件文件夹）';
+$lang['success_7'] = '我们已经将你的新密码发送到了你的邮箱！ （如果没收到请查看垃圾邮件文件夹）';
+$lang['success_8'] = '人物转生成功。';
+$lang['success_9'] = '人物数值重置成功。';
+$lang['success_10'] = '人物红名状态清除成功。';
+$lang['success_11'] = '人物位置重置成功。';
+$lang['success_12'] = '人物大师等级重置成功。';
+$lang['success_13'] = '';
+$lang['success_14'] = '';
+$lang['success_15'] = '已成功追加新闻。';
+$lang['success_16'] = '你的 master key 已成功发送至邮箱。';
+$lang['success_17'] = '加点成功。';
+$lang['success_18'] = '验证链接已经发往你的电子邮箱，请查收以继续注册。';
+$lang['success_19'] = '请检查你的电邮地址以继续。';
+$lang['success_20'] = '账户电邮地址已经成功更改。';
+$lang['success_21'] = 'Zen（金） 购买成功。';
+$lang['success_22'] = '感谢联系我们，我们将尽快回复。';
+
+# errors
+$lang['error_1'] = '您提供的密码不正确，请重试。';
+$lang['error_2'] = '您输入的用户名不存在。';
+$lang['error_3'] = '您已达到登录失败的最大数量，因此您的帐户已被暂时禁止登录该网站。';
+$lang['error_4'] = '您必须填写表格中的所有字段。';
+$lang['error_5'] = '用户名长度必须为4到10个字符。';
+$lang['error_6'] = '用户名只能包含字母数字字符。';
+$lang['error_7'] = '密码长度可以为4到32个字符。';
+$lang['error_8'] = '密码不匹配。';
+$lang['error_9'] = '您输入的电子邮件地址无效。';
+$lang['error_10'] = '您输入的用户名已经存在。';
+$lang['error_11'] = '您输入的电子邮件地址已经存在。';
+$lang['error_12'] = '无法检索您的帐户信息。请稍后再试。';
+$lang['error_13'] = '您的密码错误。';
+$lang['error_14'] = '您的帐户在线，请断开连接。';
+$lang['error_15'] = '您的帐户在过去12小时内已为此TopSite投票。';
+$lang['error_16'] = '您的IP地址在过去12小时内已为此TopSite投票。';
+$lang['error_17'] = '当前未启用新注册，请稍后再试。';
+$lang['error_18'] = 'Recaptcha 验证码验证失败，请重试。';
+$lang['error_19'] = '您有一个有效的密码更改请求，请检查您的电子邮件。';
+$lang['error_20'] = '我们无法向您发送验证电子邮件，请与支持团队联系。';
+$lang['error_21'] = '您的请求无法处理，请联系支持团队。';
+$lang['error_22'] = '我们无法创建您的帐户，请联系支持团队。';
+$lang['error_23'] = '发生意外错误，请联系支持团队。';
+$lang['error_24'] = '您的请求中提供的信息无效。';
+$lang['error_25'] = '您的请求无法完成，请重试。';
+$lang['error_26'] = '您的密码更改请求已过期，请重新申请。';
+$lang['error_27'] = '提供的授权码无效。';
+$lang['error_28'] = '您的帐户在线，请断开连接。';
+$lang['error_29'] = '您的密码无法更改，请联系支持团队。';
+$lang['error_30'] = '未使用提供的电子邮件地址找到帐户。';
+$lang['error_31'] = '无效的密码恢复数据。';
+$lang['error_32'] = '你无权进行人物转生。';
+$lang['error_33'] = '你的人物未到达人物转生所需等级。';
+$lang['error_34'] = '你的人物所携带的金不足。';
+$lang['error_35'] = '你无权重置人物点数。';
+$lang['error_36'] = '你无权清除这个人物的红名。';
+$lang['error_37'] = '你无权重置这个人物的位置。';
+$lang['error_38'] = '你无权重置这个人物的技能树。';
+$lang['error_39'] = '你的大师等级须为 ';
+$lang['error_40'] = '您的点券不足。';
+$lang['error_41'] = '请填入所有内容。';
+$lang['error_42'] = '标题至少可以包含4个字符，最多80个字符。';
+$lang['error_43'] = '新闻内容必须至少包含4个字符。';
+$lang['error_44'] = '无法加载请求的排名。';
+$lang['error_45'] = '无效的操作，重定向到首页。';
+$lang['error_46'] = '您的账户下尚无角色。';
+$lang['error_47'] = '该模块当前未启用，请稍后再试。';
+$lang['error_48'] = '由于电子邮件系统未激活，无法处理您的请求，请联系支持团队。';
+$lang['error_49'] = '您的帐户没有主密钥。';
+$lang['error_50'] = '您不到一个小时前就已经请求主密钥，请稍后再试。';
+$lang['error_51'] = '你的升级点数不足。';
+$lang['error_52'] = '只有圣导师可以加点统率点数。';
+$lang['error_53'] = '已超过最大加点数值，请重试。';
+$lang['error_54'] = '您未添加要添加的最低要求升级点： ';
+$lang['error_55'] = '你将购买的金数量超出了玩家人物所能携带的数量，请重试。';
+$lang['error_56'] = '输入的名称无效。';
+$lang['error_57'] = '输入的消息无效。';
+$lang['error_58'] = '没有排名结果可显示。';
+$lang['error_59'] = '无法确定哪些职业需要统率数据， 请在你的服务器文件中设定 character_cmd 。';
+$lang['error_60'] = '无法加载Recaptcha库，请联系支持。';
+$lang['error_61'] = '尚无新闻报道。';
+$lang['error_62'] = '语言切换系统已禁用。';
+$lang['error_63'] = '所选语言无效。';
+$lang['error_64'] = '你无权为此角色加点。';
+$lang['error_65'] = '切换语言失败。';
+
+# credit system errors
+$lang['error_66'] = '您尚未设置配置ID。';
+$lang['error_67'] = '输入的用户ID无效。';
+$lang['error_68'] = '输入的用户名包含不允许的字符。';
+$lang['error_69'] = '输入的用户名无效。';
+$lang['error_70'] = '输入的电子邮件地址无效。';
+$lang['error_71'] = '输入的字符名称无效。';
+$lang['error_72'] = '点卷添加数量必须是非带号（unsigned）整数。';
+$lang['error_73'] = '尚未设置用户标识符。';
+$lang['error_74'] = '点卷添加失败。';
+$lang['error_75'] = '点卷减少数量必须是非带号（unsigned）整数。';
+$lang['error_76'] = '点卷减少失败。';
+$lang['error_77'] = '提供的配置ID无效。';
+$lang['error_78'] = '标题只能包含字母数字字符和空格。';
+$lang['error_79'] = '输入的数据库包含不允许的字符。';
+$lang['error_80'] = '输入的表包含不允许的字符。';
+$lang['error_81'] = '输入的积分列包含不允许的字符。';
+$lang['error_82'] = '输入的用户列包含不允许的字符。';
+$lang['error_83'] = '用户列标识符无效。';
+$lang['error_84'] = '请填写所有必填字段。';
+$lang['error_85'] = '在将配置添加到数据库时发生错误，请检查数据库错误。';
+$lang['error_86'] = '编辑配置时出错，请检查数据库错误。';
+$lang['error_87'] = '删除配置时发生错误，请检查数据库错误。';
+$lang['error_88'] = '未设置标识符，无法检查在线状态。';
+$lang['error_89'] = '获取积分值时出错。';
+
+# email system errors
+$lang['error_90'] = '无法加载电子邮件配置。';
+$lang['error_91'] = '无法加载电子邮件模板。';
+$lang['error_92'] = '电子邮件地址无效，无法发送电子邮件。';
+$lang['error_93'] = '您没有设置模板。';
+$lang['error_94'] = '无法加载自定义电子邮件模板（您必须提供模板的完整路径）。';
+$lang['error_95'] = '尚未设置电子邮件模板。';
+$lang['error_96'] = '未设置收件人电子邮件地址。';
+$lang['error_97'] = '尚未设置电子邮件主题。';
+
+$lang['error_98'] = '缺少登录配置。';
+$lang['error_99'] = '编辑新闻时出错。';
+$lang['error_100'] = '无法加载投票系统配置。';
+$lang['error_101'] = 'IP地址无效。';
+$lang['error_102'] = '未设置积分点卷配置（投票系统用）。';
+
+# registration
+$lang['register_txt_1'] = '用户名：';
+$lang['register_txt_2'] = '密码：';
+$lang['register_txt_3'] = '确认密码：';
+$lang['register_txt_4'] = '电子邮件地址：';
+$lang['register_txt_5'] = '创建帐户';
+$lang['register_txt_6'] = '4到10个字母·数字字符';
+$lang['register_txt_7'] = '4到32个字符';
+$lang['register_txt_8'] = '重新输入密码';
+$lang['register_txt_9'] = '请使用有效的电子邮件地址';
+$lang['register_txt_10'] = '通过注册即表示您同意我们的<br><a href="%s" target="_blank">服务条款</a>。';
+
+# login
+$lang['login_txt_1'] = '用户名：';
+$lang['login_txt_2'] = '密码：';
+$lang['login_txt_3'] = '登录';
+$lang['login_txt_4'] = '忘记密码了？';
+$lang['login_txt_5'] = '您在％s次登录尝试中使用了％s次。 使用完所有％s次数后，您将无法登录15分钟。';
+
+# account
+$lang['myaccount_txt_1'] = '账户状态：';
+$lang['myaccount_txt_2'] = '用户名：';
+$lang['myaccount_txt_3'] = '电子邮件地址：';
+$lang['myaccount_txt_4'] = '密码：';
+$lang['myaccount_txt_5'] = '在线状态：';
+$lang['myaccount_txt_6'] = '更改';
+$lang['myaccount_txt_7'] = '已激活';
+$lang['myaccount_txt_8'] = '已冻结';
+$lang['myaccount_txt_9'] = '在线';
+$lang['myaccount_txt_10'] = '离线';
+$lang['myaccount_txt_12'] = '点卷积分：';
+$lang['myaccount_txt_13'] = 'Master Key:';
+$lang['myaccount_txt_14'] = '找回';
+$lang['myaccount_txt_15'] = '账号下人物：';
+$lang['myaccount_txt_16'] = '无';
+
+# voting
+# Not translated because none of the Chinese servers would need it - they have something entirely different.
+$lang['vfc_txt_1'] = 'TopSite';
+$lang['vfc_txt_2'] = 'Credits Reward';
+$lang['vfc_txt_3'] = 'Vote Now!';
+
+# forgot password
+$lang['forgotpass_txt_1'] = '电子邮件地址：';
+$lang['forgotpass_txt_2'] = '发送找回密码邮件';
+
+# downloads
+$lang['downloads_txt_1'] = '文件';
+$lang['downloads_txt_2'] = '载点';
+$lang['downloads_txt_3'] = '文件大小';
+$lang['downloads_txt_4'] = 'MB';
+$lang['downloads_txt_5'] = '下载';
+$lang['downloads_txt_6'] = '客户端下载';
+$lang['downloads_txt_7'] = '补丁下载';
+$lang['downloads_txt_8'] = '工具下载';
+
+# news
+$lang['news_txt_1'] = 'By %s, %s';
+$lang['news_txt_2'] = '显示评论';
+$lang['news_txt_3'] = '更多 &rsaquo;';
+
+# rankings
+$lang['rankings_txt_1'] = '最高等级';
+$lang['rankings_txt_2'] = '最高转生';
+$lang['rankings_txt_3'] = '最多PK';
+$lang['rankings_txt_4'] = '最强公会';
+$lang['rankings_txt_5'] = '最多大转生';
+$lang['rankings_txt_6'] = '在线时间最长';
+$lang['rankings_txt_7'] = '投票最多';
+$lang['rankings_txt_8'] = '最强家族';
+$lang['rankings_txt_9'] = 'PVP荣耀';
+$lang['rankings_txt_10'] = '人物';
+$lang['rankings_txt_11'] = '职业';
+$lang['rankings_txt_12'] = '等级';
+$lang['rankings_txt_13'] = '转生';
+$lang['rankings_txt_14'] = 'PK数';
+$lang['rankings_txt_15'] = '总在线时间';
+$lang['rankings_txt_16'] = '小时';
+$lang['rankings_txt_17'] = '公会名称';
+$lang['rankings_txt_18'] = '公会主';
+$lang['rankings_txt_19'] = '得分';
+$lang['rankings_txt_20'] = '最终更新 @';
+$lang['rankings_txt_21'] = '大转生';
+$lang['rankings_txt_22'] = '最高大师等级';
+$lang['rankings_txt_23'] = '大师等级';
+$lang['rankings_txt_24'] = 'PvP荣耀';
+$lang['rankings_txt_25'] = '胜利次数';
+$lang['rankings_txt_26'] = 'Duprian家族';
+$lang['rankings_txt_27'] = 'Vanert家族';
+$lang['rankings_txt_28'] = '公会徽章';
+$lang['rankings_txt_29'] = '类别';
+$lang['rankings_txt_30'] = '等级';
+$lang['rankings_txt_31'] = '贡献';
+$lang['rankings_txt_32'] = '投票';
+$lang['rankings_txt_33'] = '国家';
+$lang['rankings_txt_34'] = '地区';
+$lang['rankings_txt_35'] = 'PK等级';
+
+# clear pk
+$lang['clearpk_txt_1'] = '人物';
+$lang['clearpk_txt_2'] = '金';
+$lang['clearpk_txt_3'] = '杀人数';
+$lang['clearpk_txt_4'] = '清除红名';
+$lang['clearpk_txt_5'] = '清除红名的费用是 %s 金，请确保角色身上必须有这个数目。';
+
+# clear skill tree
+$lang['clearst_txt_1'] = '人物';
+$lang['clearst_txt_2'] = '大师等级';
+$lang['clearst_txt_3'] = '金';
+$lang['clearst_txt_4'] = 'GO';
+$lang['clearst_txt_5'] = '点数';
+$lang['clearst_txt_6'] = '你的人物必须在大师等级%s';
+$lang['clearst_txt_7'] = '重置技能树的费用是 %s 金，请确保角色身上有这个数目。';
+
+# donation
+$lang['donation_txt_2'] = '点卷积分';
+
+# change password
+$lang['changepassword_txt_1'] = '旧密码：';
+$lang['changepassword_txt_2'] = '新密码：';
+$lang['changepassword_txt_3'] = '确认新密码：';
+$lang['changepassword_txt_4'] = '更改密码';
+
+# reset
+$lang['resetcharacter_txt_1'] = '人物';
+$lang['resetcharacter_txt_2'] = '等级';
+$lang['resetcharacter_txt_3'] = '金';
+$lang['resetcharacter_txt_4'] = '转生数';
+$lang['resetcharacter_txt_5'] = '转生';
+$lang['resetcharacter_txt_6'] = '你的人物等级必须是 %s';
+$lang['resetcharacter_txt_7'] = '转生费用是 %s 金，请确保角色身上有这个数目。';
+$lang['resetcharacter_txt_8'] = '获得了 %s 积分点卷奖励！';
+
+# fix stats
+$lang['resetstats_txt_1'] = '人物';
+$lang['resetstats_txt_2'] = '等级';
+$lang['resetstats_txt_3'] = '力量';
+$lang['resetstats_txt_4'] = '敏捷';
+$lang['resetstats_txt_5'] = '体力';
+$lang['resetstats_txt_6'] = '精神';
+$lang['resetstats_txt_7'] = '统率';
+$lang['resetstats_txt_8'] = '修正';
+$lang['resetstats_txt_9'] = '人物点数修正费用是 %s 金，请确保角色身上有这个数目。';
+
+# unstick
+$lang['unstickcharacter_txt_1'] = '人物';
+$lang['unstickcharacter_txt_2'] = '金';
+$lang['unstickcharacter_txt_3'] = '位置重置';
+$lang['unstickcharacter_txt_4'] = '人物位置重置费用是 %s 金，请确保角色身上有这个数目。';
+
+# recover master key
+$lang['masterkey_txt_1'] = '将 master key 发往我的邮箱！';
+
+# add stats
+$lang['addstats_txt_1'] = '人物';
+$lang['addstats_txt_2'] = '可加点数 <strong>%s</strong> 点';
+$lang['addstats_txt_3'] = '力量';
+$lang['addstats_txt_4'] = '敏捷';
+$lang['addstats_txt_5'] = '体力';
+$lang['addstats_txt_6'] = '精神';
+$lang['addstats_txt_7'] = '统率';
+$lang['addstats_txt_8'] = '加点';
+$lang['addstats_txt_9'] = '加点费用为 %s 金，请确保角色身上有这个数目。';
+
+# castle siege banner
+$lang['csbanner_txt_1'] = '城堡拥有者';
+$lang['csbanner_txt_2'] = '离下一轮攻城战';
+$lang['csbanner_txt_3'] = '公会主';
+
+# profiles
+$lang['profiles_txt_1'] = '公会数据';
+$lang['profiles_txt_2'] = '玩家数据';
+$lang['profiles_txt_3'] = '公会主：';
+$lang['profiles_txt_4'] = '分数：';
+$lang['profiles_txt_5'] = '成员：';
+$lang['profiles_txt_6'] = '成员';
+$lang['profiles_txt_7'] = '等级';
+$lang['profiles_txt_8'] = '转生';
+$lang['profiles_txt_9'] = '大转生';
+$lang['profiles_txt_10'] = '力量';
+$lang['profiles_txt_11'] = '敏捷';
+$lang['profiles_txt_12'] = '体力';
+$lang['profiles_txt_13'] = '精神';
+$lang['profiles_txt_14'] = '统率';
+$lang['profiles_txt_15'] = '杀人数';
+$lang['profiles_txt_16'] = '公会';
+$lang['profiles_txt_17'] = '状态';
+$lang['profiles_txt_18'] = '在线';
+$lang['profiles_txt_19'] = '离线';
+$lang['profiles_txt_20'] = '大师等级';
+
+# change email address
+$lang['changemail_txt_1'] = '新电子邮箱地址';
+$lang['changemail_txt_2'] = '更改';
+
+# sidebar server info
+$lang['sidebar_srvinfo_txt_1'] = '服务器信息';
+$lang['sidebar_srvinfo_txt_2'] = '账户总数';
+$lang['sidebar_srvinfo_txt_3'] = '人物总数';
+$lang['sidebar_srvinfo_txt_4'] = '公会总数';
+$lang['sidebar_srvinfo_txt_5'] = '在线人数';
+
+# castle siege
+$lang['castlesiege_txt_2'] = '城堡拥有者：';
+$lang['castlesiege_txt_3'] = '金：';
+$lang['castlesiege_txt_4'] = '玛雅合成税率：';
+$lang['castlesiege_txt_5'] = '商店税率：';
+$lang['castlesiege_txt_6'] = '猎区税率';
+$lang['castlesiege_txt_7'] = '已注册公会';
+
+# buy zen
+$lang['buyzen_txt_1'] = '购买金';
+$lang['buyzen_txt_2'] = ' 金 送往 ';
+$lang['buyzen_txt_3'] = '人物';
+$lang['buyzen_txt_4'] = '金';
+$lang['buyzen_txt_5'] = '购买';
+$lang['buyzen_txt_6'] = '点卷积分';
+
+# contact us
+$lang['contactus_txt_1'] = '电子邮件地址';
+$lang['contactus_txt_2'] = '信息';
+$lang['contactus_txt_3'] = '发送';

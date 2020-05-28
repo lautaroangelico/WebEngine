bade17dc7f3a988e9cad09cb8ab358be599a523a
Lautaro Angelico <angelico.lautaro@gmail.com>
2020-05-28 04:12:05 -0700

    greatly improved online characters cron

--- includes/cron/online_characters.php
+++ includes/cron/online_characters.php
@@ -3,9 +3,9 @@
  * WebEngine CMS
  * https://webenginecms.org/
  * 
- * @version 1.2.0
+ * @version 1.2.1
  * @author Lautaro Angelico <http://lautaroangelico.com/>
- * @copyright (c) 2013-2019 Lautaro Angelico, All Rights Reserved
+ * @copyright (c) 2013-2020 Lautaro Angelico, All Rights Reserved
  * 
  * Licensed under the MIT license
  * http://opensource.org/licenses/MIT
@@ -16,19 +16,24 @@ $file_name = basename(__FILE__);
 
 // load database
 $me = Connection::Database('Me_MuOnline');
+$mu = Connection::Database('MuOnline');
 
 $onlineAccounts = $me->query_fetch("SELECT "._CLMN_MS_MEMBID_." FROM "._TBL_MS_." WHERE "._CLMN_CONNSTAT_." = ?", array(1));
-if(is_array($onlineAccounts)) {
-	$Character = new Character();
-	foreach($onlineAccounts as $row) {
-		$characterIDC = $Character->AccountCharacterIDC($row[_CLMN_MS_MEMBID_]);
-		if(!check_value($characterIDC)) continue;
-		$result[] = $characterIDC;
-	}
-	
-	$cacheData = encodeCache($result);
-	updateCacheFile('online_characters.cache', $cacheData);
+foreach($onlineAccounts as $row) {
+	$onlineAccountList[] = '\''.utf8_encode($row[_CLMN_MS_MEMBID_]).'\'';
 }
+
+$result = array();
+$onlineAccountListString = implode(',', $onlineAccountList);
+$characterIDC = $mu->query_fetch("SELECT "._CLMN_GAMEIDC_." FROM "._TBL_AC_." WHERE "._CLMN_AC_ID_." IN(".$onlineAccountListString.")");
+if(is_array($characterIDC)) {
+	foreach($characterIDC as $idc) {
+		$result[] = $idc[_CLMN_GAMEIDC_];
+	}
+}
+
+$cacheData = encodeCache($result);
+updateCacheFile('online_characters.cache', $cacheData);
 
 // UPDATE CRON
 updateCronLastRun($file_name);

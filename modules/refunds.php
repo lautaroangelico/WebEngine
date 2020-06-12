31e2a3c03a7f9348efd7b33aea6daffee468fa4f
Lautaro Angelico <angelico.lautaro@gmail.com>
2020-06-12 01:19:02 -0700

    Refund policy module created

---
+++ modules/refunds.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * WebEngine CMS
+ * https://webenginecms.org/
+ * 
+ * @version 1.2.1
+ * @author Lautaro Angelico <http://lautaroangelico.com/>
+ * @copyright (c) 2013-2020 Lautaro Angelico, All Rights Reserved
+ * 
+ * Licensed under the MIT license
+ * http://opensource.org/licenses/MIT
+ */
+?>
+
+<div class="page-title">
+	<span>
+		<?php echo lang('module_titles_txt_27'); ?>
+	</span>
+</div>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie ultricies sem, ac maximus felis suscipit at. Phasellus eu hendrerit arcu, quis finibus turpis. Sed efficitur, libero vel aliquam mollis, quam justo gravida est, vel ornare metus diam vitae eros. Morbi et condimentum libero. In quis sodales nisi, nec efficitur ipsum.</p>
+
+<h3>Nunc velit tellus</h3>
+<p>Vivamus vitae massa eget risus dapibus consectetur. Etiam eu sagittis magna. Donec facilisis dolor erat, at consequat ligula tincidunt imperdiet. Nulla facilisi. Suspendisse varius odio tincidunt finibus convallis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent aliquet commodo odio, ut elementum massa congue eu. Aenean magna sapien, sollicitudin in diam non, molestie tristique dolor. Nam a felis fringilla, rhoncus ex et, mattis purus. Etiam vitae porttitor lacus. Nam molestie, massa nec blandit gravida, velit neque porttitor augue, et eleifend tellus ex et eros. Sed nibh tortor, pellentesque nec ullamcorper ac, convallis ut mi. Nunc maximus posuere rhoncus.</p>
+
+<h3>Donec id facilisis nulla</h3>
+<p>Donec vel erat ut nulla porttitor iaculis eu ac lacus. Curabitur vitae justo efficitur, posuere nunc ut, aliquet eros. Donec pulvinar auctor dui.</p>
+<ol>
+	<li>Suspendisse quis ex ac felis mollis lobortis</li>
+	<li>Duis dapibus ornare rhoncus</li>
+	<li>Praesent vehicula scelerisque lorem dictum tincidunt</li>
+	<li>Rutrum risus tempus in</li>
+</ol>
